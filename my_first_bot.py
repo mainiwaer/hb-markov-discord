497d9ae80ad8474f1d5380d69fe3b2d69830bae6
vagrant <munawaer.ainiwaer@gmail.com>
2021-03-04 00:29:32 +0000

    Completed code inside of my_first_bot to enable markov chain in markov.py to run.
    Co-authored-by: Missy Davies <ms.melissadavies@gmail.com>

--- my_first_bot.py
+++ my_first_bot.py
@@ -1,6 +1,7 @@
 import discord
 import os 
 import markov
+import sys
 
 client = discord.Client()
 
@@ -13,14 +14,11 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('hello'):
-        filename = markov.open_and_read_file("green-eggs.txt")
-        markov_chains = markov.make_chains(filename)
-        makov_text = markov.make_text(markov_chains)
+    if message.content.startswith('ham me'):
+        filename = sys.argv[1:]
+        raw_text = markov.open_and_read_file(filename)
+        markov_chains = markov.make_chains(raw_text)
+        markov_text = markov.make_text(markov_chains)
         await message.channel.send(markov_text)
-
-        # open_and_read_file
-        # make_chains
-        # make_text
 
 client.run(os.environ['DISCORD_TOKEN'])
